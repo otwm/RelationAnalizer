948fda0270e883ca8d186d6ad33281710efa7a66
kdo <otwmoo@gmail.com>
2015-10-30 08:00:49 +0900

    initial

---
+++ src/main/java/com/kdo/relationalanallizer/antlr4/Antlr4Analizer.java
@@ -0,0 +1,10 @@
+package com.kdo.relationalanallizer.antlr4;
+
+/**
+ * Created by kdo on 2015-10-30.
+ */
+public class Antlr4Analizer {
+    public void dummy(){
+        
+    }
+}
--- src/main/java/com/kdo/relationalanallizer/result/Relation.java
+++
@@ -1,8 +0,0 @@
-package com.kdo.relationalanallizer.result;
-
-/**
- * Created by kdo on 2015-10-27.
- */
-public enum Relation {
-    OneToOne, OneToMany, ManyToMany;
-}
